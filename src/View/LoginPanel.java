0969ab679b329d5864ed274d2f0aaca7e76eeb9e
saf65 <646safahos@gmail.com>
2026-06-01 23:38:30 +0600

    Added

--- src/View/LoginPanel.java
+++ src/View/LoginPanel.java
@@ -6,7 +6,6 @@ import java.awt.*;
 public class LoginPanel extends JPanel {
     private final JTextField userField = new JTextField(15);
     private final JPasswordField passField = new JPasswordField(15);
-    private final JButton loginBtn = new JButton("Login");
 
     public LoginPanel(Runnable onLoginSuccess) {
         setLayout(new GridBagLayout());
@@ -18,7 +17,9 @@ public class LoginPanel extends JPanel {
         gbc.gridx = 1; add(userField, gbc);
         gbc.gridx = 0; gbc.gridy = 1; add(new JLabel("Password:"), gbc);
         gbc.gridx = 1; add(passField, gbc);
-        gbc.gridx = 1; gbc.gridy = 2; add(loginBtn, gbc);
+        gbc.gridx = 1; gbc.gridy = 2;
+        JButton loginBtn = new JButton("Login");
+        add(loginBtn, gbc);
 
         loginBtn.addActionListener(e -> {
             String username = userField.getText().trim();
